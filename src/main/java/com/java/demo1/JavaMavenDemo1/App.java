719ed7165e6c296f6faea85d5a0d21ffc5308fe1
VenuMadhav21 <venumadhav721@gmail.com>
2024-11-14 15:52:57 +0530

    added folders

---
+++ src/main/java/com/java/demo1/JavaMavenDemo1/App.java
@@ -0,0 +1,13 @@
+package com.java.demo1.JavaMavenDemo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Venu Madhav - 22BD1A054B" );
+    }
+}
